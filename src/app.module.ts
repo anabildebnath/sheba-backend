ac58166e6bbe00508feb19c7d7405d211a477671
Anabil Debnath <73880095+anabildebnath@users.noreply.github.com>
2025-05-09 22:26:52 +0600

    docker-compose resolves all issues-redis,postgres,api container started

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookingsModule } from './bookings/bookings.module';
@@ -8,7 +9,10 @@ import * as process from 'process';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true, // Ensure the config is available globally
+      envFilePath: '.env', // Explicitly specify your environment file
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST || 'localhost',
@@ -16,8 +20,8 @@ import * as process from 'process';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [],
       synchronize: true,
+      autoLoadEntities: true, // Automatically load entities, important for TypeORM to work correctly
     }),
     BookingsModule,
     ServicesModule,
